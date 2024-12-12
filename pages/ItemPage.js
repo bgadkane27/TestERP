90712891a30adf5e25b6f94716110761ba7be33d
Baburao Adkane <b.g.adkane27@gmail.com>
2024-12-12 12:43:08 +0530

    dataprovide

--- pages/ItemPage.js
+++ pages/ItemPage.js
@@ -8,7 +8,7 @@ class ItemPage {
         this.itemnamearabic = Selector("input[name='NameL2']")
         this.savebutton = Selector("#MainMenu_DXI0_T, #MainMenu_DXI0_Img")
         this.iteminput = Selector('input[aria-label="Filter cell"][role="textbox"][aria-describedby="dx-col-3"]')
-        this.selectitem= Selector(".list-hyperlink").withExactText("Weight")
+        //this.selectitem= Selector(".list-hyperlink").withExactText("Weight")
         this.itemcontextmenu = Selector("#MainMenu_DXI18_P")
         this.deletebutton = Selector("#MainMenu_DXI4_T")
         this.deleteokbutton = Selector('.dx-button-content .dx-button-text').withText('Ok') 
@@ -27,10 +27,11 @@ class ItemPage {
     }
 
     async deleteItem(name){
+        const selectitem = Selector(".list-hyperlink").withExactText(name);
         await t
         .typeText(this.iteminput, name)
         .wait(2000)
-        .click(this.selectitem)
+        .click(selectitem)
         .click(this.itemcontextmenu)
         .click(this.deletebutton)
         .click(this.deleteokbutton)
--- tests/Item/ItemTests.js
+++ tests/Item/ItemTests.js
@@ -1,8 +1,169 @@
+// import { Selector, Role, fixture, t } from "testcafe";
+// import LoginPage from "../../pages/LoginPage";
+// import ItemPage from "../../pages/ItemPage";
+// const config = require("../../utils/login.json");
+// const item = require("../../utils/item.json");
+// import {
+//   assertItemCreated,
+//   assertItemDuplicate,
+//   assertItemDelete,
+//   assertItemUpdate,
+// } from "../../assertions/itemAssertions";
+
+// const login = Role(
+//   "./",
+//   async (t) => {
+//     await t.maximizeWindow();
+//     await LoginPage.login(
+//       config.logincredentials.username,
+//       config.logincredentials.password
+//     );
+//   },
+//   { preserveUrl: true }
+// );
+
+// async function handleDropdownofUOM(dropdownSelector, optionText) {
+//   const dropdownType = Selector(dropdownSelector).nth(9);
+//   const dropdownOptions = Selector(
+//     ".dx-item-content, .dx-list-item-content"
+//   ).withText(optionText);
+
+//   await t.click(dropdownType).click(dropdownOptions);
+// }
+
+// async function handleDropdownofItemType(dropdownSelector, optionText) {
+//   const dropdownType = Selector(dropdownSelector).nth(10);
+//   const dropdownOptions = Selector(
+//     ".dx-item-content, .dx-list-item-content"
+//   ).withText(optionText);
+
+//   await t.click(dropdownType).click(dropdownOptions);
+// }
+
+// async function handleDropdownofTrackingMode(dropdownSelector, optionText) {
+//   const dropdownType = Selector(dropdownSelector).nth(11);
+//   const dropdownOptions = Selector(
+//     ".dx-item-content, .dx-list-item-content"
+//   ).withText(optionText);
+
+//   await t.click(dropdownType).click(dropdownOptions);
+// }
+
+// async function handleDropdownofCostingMethod(dropdownSelector, optionText) {
+//   const dropdownType = Selector(dropdownSelector).nth(12);
+//   const dropdownOptions = Selector(
+//     ".dx-item-content, .dx-list-item-content"
+//   ).withText(optionText);
+
+//   await t.click(dropdownType).click(dropdownOptions);
+// }
+
+// fixture("Item Tests").page("./");
+
+// test(
+//   "CreateNewItem: " +
+//     item.new.name1 +
+//     " UOM :" +
+//     item.new.UOM +
+//     " Item Type: " +
+//     item.new.Type +
+//     " Tracking Mode: " +
+//     item.new.TrackingMode +
+//     " Costing Method: " +
+//     item.new.CostingMethod1,
+//   async (t) => {
+//     await t.useRole(login);
+//     await ItemPage.navigateToItemSection();
+//     await ItemPage.createItem(item.new.name1, item.new.arabicname);
+//     await handleDropdownofUOM("div.dx-button-content", item.new.UOM); // Provide UOM
+//     await handleDropdownofItemType(".dx-button-content", item.new.Type); // Provide Item Type
+//     await handleDropdownofTrackingMode(
+//       "div.dx-button-content",
+//       item.new.TrackingMode
+//     ); // Provide Tracking Mode
+//     await handleDropdownofCostingMethod(
+//       "div.dx-button-content",
+//       item.new.CostingMethod1
+//     ); // Provide Costing Method
+//     await t.click("#MainMenu_DXI0_T");
+//     await assertItemCreated(t, item.new.name1);
+//   }
+// );
+
+// test(
+//     "CreateNewItem: " +
+//       item.new.name2 +
+//       " UOM :" +
+//       item.new.UOM +
+//       " Item Type: " +
+//       item.new.Type +
+//       " Tracking Mode: " +
+//       item.new.TrackingMode +
+//       " Costing Method: " +
+//       item.new.CostingMethod2,
+//     async (t) => {
+//       await t.useRole(login);
+//       await ItemPage.navigateToItemSection();
+//       await ItemPage.createItem(item.new.name2, item.new.arabicname);
+//       await handleDropdownofUOM("div.dx-button-content", item.new.UOM); // Provide UOM
+//       await handleDropdownofItemType(".dx-button-content", item.new.Type); // Provide Item Type
+//       await handleDropdownofTrackingMode(
+//         "div.dx-button-content",
+//         item.new.TrackingMode
+//       ); // Provide Tracking Mode
+//       await handleDropdownofCostingMethod(
+//         "div.dx-button-content",
+//         item.new.CostingMethod2
+//       ); // Provide Costing Method
+//       await t.click("#MainMenu_DXI0_T");
+//       await assertItemCreated(t, item.new.name2);
+//     }
+// );
+
+// test(
+//     "CreateNewItem: " +
+//       item.new.name3 +
+//       " UOM :" +
+//       item.new.UOM +
+//       " Item Type: " +
+//       item.new.Type +
+//       " Tracking Mode: " +
+//       item.new.TrackingMode +
+//       " Costing Method: " +
+//       item.new.CostingMethod3,
+//     async (t) => {
+//       await t.useRole(login);
+//       await ItemPage.navigateToItemSection();
+//       await ItemPage.createItem(item.new.name3, item.new.arabicname);
+//       await handleDropdownofUOM("div.dx-button-content", item.new.UOM); // Provide UOM
+//       await handleDropdownofItemType(".dx-button-content", item.new.Type); // Provide Item Type
+//       await handleDropdownofTrackingMode(
+//         "div.dx-button-content",
+//         item.new.TrackingMode
+//       ); // Provide Tracking Mode
+//       await handleDropdownofCostingMethod(
+//         "div.dx-button-content",
+//         item.new.CostingMethod3
+//       ); // Provide Costing Method
+//       await t.click("#MainMenu_DXI0_T");
+//       await assertItemCreated(t, item.new.name3);
+//     }
+// ); 
+
+
+// test("DeleteExistingItem: " + item.delete.name, async (t) => {
+//   await t.useRole(login);
+//   await t.wait(2000).click("#leftNavigation_I0i2_");
+//   await ItemPage.deleteItem(item.delete.name);
+//   await assertItemDelete(t, "Item deleted successfully");
+// });
+
+
 import { Selector, Role, fixture, t } from "testcafe";
 import LoginPage from "../../pages/LoginPage";
 import ItemPage from "../../pages/ItemPage";
 const config = require("../../utils/login.json");
-const item = require("../../utils/item.json");
+const items = require("../../utils/item.json");
 import {
   assertItemCreated,
   assertItemDuplicate,
@@ -22,35 +183,8 @@ const login = Role(
   { preserveUrl: true }
 );
 
-async function handleDropdownofUOM(dropdownSelector, optionText) {
-  const dropdownType = Selector(dropdownSelector).nth(9);
-  const dropdownOptions = Selector(
-    ".dx-item-content, .dx-list-item-content"
-  ).withText(optionText);
-
-  await t.click(dropdownType).click(dropdownOptions);
-}
-
-async function handleDropdownofItemType(dropdownSelector, optionText) {
-  const dropdownType = Selector(dropdownSelector).nth(10);
-  const dropdownOptions = Selector(
-    ".dx-item-content, .dx-list-item-content"
-  ).withText(optionText);
-
-  await t.click(dropdownType).click(dropdownOptions);
-}
-
-async function handleDropdownofTrackingMode(dropdownSelector, optionText) {
-  const dropdownType = Selector(dropdownSelector).nth(11);
-  const dropdownOptions = Selector(
-    ".dx-item-content, .dx-list-item-content"
-  ).withText(optionText);
-
-  await t.click(dropdownType).click(dropdownOptions);
-}
-
-async function handleDropdownofCostingMethod(dropdownSelector, optionText) {
-  const dropdownType = Selector(dropdownSelector).nth(12);
+async function handleDropdown(dropdownSelector, optionText, nthIndex) {
+  const dropdownType = Selector(dropdownSelector).nth(nthIndex);
   const dropdownOptions = Selector(
     ".dx-item-content, .dx-list-item-content"
   ).withText(optionText);
@@ -60,101 +194,31 @@ async function handleDropdownofCostingMethod(dropdownSelector, optionText) {
 
 fixture("Item Tests").page("./");
 
-test(
-  "CreateNewItem: " +
-    item.new.name1 +
-    " UOM :" +
-    item.new.UOM +
-    " Item Type: " +
-    item.new.Type +
-    " Tracking Mode: " +
-    item.new.TrackingMode +
-    " Costing Method: " +
-    item.new.CostingMethod1,
-  async (t) => {
-    await t.useRole(login);
-    await ItemPage.navigateToItemSection();
-    await ItemPage.createItem(item.new.name1, item.new.arabicname);
-    await handleDropdownofUOM("div.dx-button-content", item.new.UOM); // Provide UOM
-    await handleDropdownofItemType(".dx-button-content", item.new.Type); // Provide Item Type
-    await handleDropdownofTrackingMode(
-      "div.dx-button-content",
-      item.new.TrackingMode
-    ); // Provide Tracking Mode
-    await handleDropdownofCostingMethod(
-      "div.dx-button-content",
-      item.new.CostingMethod1
-    ); // Provide Costing Method
-    await t.click("#MainMenu_DXI0_T");
-    await assertItemCreated(t, item.new.name1);
-  }
-);
-
-test(
-    "CreateNewItem: " +
-      item.new.name2 +
-      " UOM :" +
-      item.new.UOM +
-      " Item Type: " +
-      item.new.Type +
-      " Tracking Mode: " +
-      item.new.TrackingMode +
-      " Costing Method: " +
-      item.new.CostingMethod2,
+items.new.forEach((item) => {
+  test(
+    `CreateNewItem: ${item.name} UOM: ${item.UOM} Item Type: ${item.Type} Tracking Mode: ${item.TrackingMode} Costing Method: ${item.CostingMethod}`,
     async (t) => {
       await t.useRole(login);
       await ItemPage.navigateToItemSection();
-      await ItemPage.createItem(item.new.name2, item.new.arabicname);
-      await handleDropdownofUOM("div.dx-button-content", item.new.UOM); // Provide UOM
-      await handleDropdownofItemType(".dx-button-content", item.new.Type); // Provide Item Type
-      await handleDropdownofTrackingMode(
-        "div.dx-button-content",
-        item.new.TrackingMode
-      ); // Provide Tracking Mode
-      await handleDropdownofCostingMethod(
-        "div.dx-button-content",
-        item.new.CostingMethod2
-      ); // Provide Costing Method
+      await ItemPage.createItem(item.name, item.arabicname);
+      await handleDropdown("div.dx-button-content", item.UOM, 9); // Provide UOM
+      await handleDropdown("div.dx-button-content", item.Type, 10); // Provide Item Type
+      await handleDropdown("div.dx-button-content", item.TrackingMode, 11); // Provide Tracking Mode
+      await handleDropdown("div.dx-button-content", item.CostingMethod, 12); // Provide Costing Method
       await t.click("#MainMenu_DXI0_T");
-      await assertItemCreated(t, item.new.name2);
+      await assertItemCreated(t, item.name);
     }
-);
-
-test(
-    "CreateNewItem: " +
-      item.new.name3 +
-      " UOM :" +
-      item.new.UOM +
-      " Item Type: " +
-      item.new.Type +
-      " Tracking Mode: " +
-      item.new.TrackingMode +
-      " Costing Method: " +
-      item.new.CostingMethod3,
-    async (t) => {
-      await t.useRole(login);
-      await ItemPage.navigateToItemSection();
-      await ItemPage.createItem(item.new.name3, item.new.arabicname);
-      await handleDropdownofUOM("div.dx-button-content", item.new.UOM); // Provide UOM
-      await handleDropdownofItemType(".dx-button-content", item.new.Type); // Provide Item Type
-      await handleDropdownofTrackingMode(
-        "div.dx-button-content",
-        item.new.TrackingMode
-      ); // Provide Tracking Mode
-      await handleDropdownofCostingMethod(
-        "div.dx-button-content",
-        item.new.CostingMethod3
-      ); // Provide Costing Method
-      await t.click("#MainMenu_DXI0_T");
-      await assertItemCreated(t, item.new.name3);
-    }
-); 
-
-
-test("DeleteExistingItem: " + item.delete.name, async (t) => {
-  await t.useRole(login);
-  await t.wait(2000).click("#leftNavigation_I0i2_");
-  await ItemPage.deleteItem(item.delete.name);
-  await assertItemDelete(t, "Item deleted successfully");
+  );
 });
-    
+
+items.delete.forEach((item)=>{
+  test(
+    `DeleteNewlyCreated Item: ${item.name}`,
+    async(t)=>{
+      await t.useRole(login);
+      await t.wait(2000).click("#leftNavigation_I0i2_");
+      await ItemPage.deleteItem(item.name);
+      await assertItemDelete(t, "Item deleted successfully");
+    }
+  );
+})
